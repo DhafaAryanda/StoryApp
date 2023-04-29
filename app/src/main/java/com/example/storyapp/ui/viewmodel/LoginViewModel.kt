fbfa8b0ffb87caf0548f15cbea7ba8c3f1b07b90
Dhafa Aryanda <dhofaaryanda4@gmail.com>
2023-04-29 23:04:51 +0700

    Add RegisterViewModel, and LoginViewModel

--- app/src/main/java/com/example/storyapp/ui/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/storyapp/ui/viewmodel/LoginViewModel.kt
@@ -2,43 +2,45 @@ package com.example.storyapp.ui.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.storyapp.api.ApiConfig
-import com.example.storyapp.api.RegisterResponse
+import com.example.storyapp.api.LoginResponse
 import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
 
-class RegisterViewModel {
+class LoginViewModel : ViewModel() {
     private val _message = MutableLiveData<String>()
     val message: LiveData<String> = _message
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
 
+    private val _userLogin = MutableLiveData<LoginResponse>()
+    val userlogin: LiveData<LoginResponse> = _userLogin
+
     var isError: Boolean = false
 
-    fun getRegisterUser(name: String, email: String, password: String) {
+    fun getLoginUser(email: String, password: String) {
         _isLoading.value = true
-        val api = ApiConfig.getApiService().registerUser(name, email, password)
-        api.enqueue(object : Callback<RegisterResponse> {
-            override fun onResponse(
-                call: Call<RegisterResponse>,
-                response: Response<RegisterResponse>
-            ) {
+        val api = ApiConfig.getApiService().loginUser(email, password)
+        api.enqueue(object : retrofit2.Callback<LoginResponse> {
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
                 _isLoading.value = false
                 val responseBody = response.body()
                 if (response.isSuccessful) {
                     isError = false
-                    _message.value = responseBody?.message.toString()
+                    _userLogin.value = responseBody!!
+                    _message.value = "Login as ${_userLogin.value!!.loginResult.name}"
                 } else {
                     isError = true
                     _message.value = response.message()
                 }
             }
 
-            override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
                 isError = true
                 _isLoading.value = false
+                _message.value = t.message.toString()
             }
         })
     }
